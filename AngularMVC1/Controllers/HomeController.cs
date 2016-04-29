15c9af386327a21f55c5b139d3a93c0d912602db
AngJSofJJ <jayanthi.prasannas@gmail.com>
2016-04-29 10:37:03 -0700

    first commit

---
+++ AngularMVC1/Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularMVC1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetProducts()
+        {
+            try
+            {
+                var db = new ProductsDBEntities();
+                var products = db.Products.ToList();
+                return Json(products, JsonRequestBehavior.AllowGet);
+            }
+            catch (DbEntityValidationException e)
+            {
+                //foreach (var eve in e.EntityValidationErrors)
+                //{
+                //    Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                //        eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                //    foreach (var ve in eve.ValidationErrors)
+                //    {
+                //        Debug.WriteLine("- Property: \"{0}\", Value: \"{1}\", Error: \"{2}\"",
+                //         ve.PropertyName,
+                //             eve.Entry.CurrentValues.GetValue<object>(ve.PropertyName),
+                //             ve.ErrorMessage);
+                //    }
+                //}
+                throw e;
+            }
+        }
+
+        [HttpPost]
+        public JsonResult AddProduct(string newProduct, string newProductPrice, string newProductType)
+        {
+            try
+            {
+                var db = new ProductsDBEntities();
+                db.Products.Add(new Product() { ProductName = newProduct, Price = float.Parse(newProductPrice), ProductType = newProductType });
+                db.SaveChanges();
+                var products = db.Products.ToList();
+                return Json(products, JsonRequestBehavior.AllowGet);
+            }
+            catch (DbEntityValidationException e)
+            {
+                //foreach (var eve in e.EntityValidationErrors)
+                //{
+                //    Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                //        eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                //    foreach (var ve in eve.ValidationErrors)
+                //    {
+                //       Debug.WriteLine ("- Property: \"{0}\", Value: \"{1}\", Error: \"{2}\"",
+                //        ve.PropertyName,
+                //            eve.Entry.CurrentValues.GetValue<object>(ve.PropertyName),
+                //            ve.ErrorMessage);
+                //    }
+                //}
+                throw e;
+            }
+        }
+
+        [HttpPost]
+        public JsonResult DeleteProduct(Product delProduct)
+        {
+            var db = new ProductsDBEntities();
+            var product = db.Products.Find(delProduct.Id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            var products = db.Products.ToList();
+            return Json(products, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ AngularMVC1/Scripts/App.js
@@ -0,0 +1,39 @@
+﻿
+var myApp = angular.module('myApp', []);
+
+myApp.controller('mainController', function ($scope, $http) {
+    $http.get('/Home/GetProducts')
+    .success(function (result) {
+        $scope.products = result;
+    })
+    .error(function (data) {
+        console.log(data);
+    });
+
+
+    $scope.newProduct = "";
+    $scope.newProductPrice = "";
+    $scope.newProductType = "";
+    $scope.addProduct = function () {
+        $http.post('/Home/AddProduct/', { newProduct: $scope.newProduct, newProductPrice: $scope.newProductPrice, newProductType :$scope.newProductType })
+        .success(function (result) {
+            $scope.products = result;
+            $scope.newProduct = "";
+            $scope.newProductPrice = "";
+            $scope.newProductType = "";
+        })
+        .error(function (data) {
+            console.log(data);
+        });
+    }
+
+    $scope.deleteProduct = function (product) {
+        $http.post('/Home/DeleteProduct/', { delProduct: product })
+        .success(function (result) {
+            $scope.products = result;
+        })
+        .error(function (data) {
+            console.log(data);
+        });
+    }
+});
